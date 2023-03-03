002fd9a25e6e018204b44383d57049068ab30216
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-03-04 01:55:52 +0700

    detail content component

--- src/component/DetailContent/detailContent.jsx
+++ src/component/DetailContent/detailContent.jsx
@@ -1,38 +1,36 @@
 import { Button, Statistic, Rate } from "antd";
-import { CaretRightOutlined, PlusOutlined, LikeOutlined} from '@ant-design/icons';
+import { CaretRightOutlined, PlusOutlined, LikeFilled, StarFilled, StarOutlined, StarTwoTone} from '@ant-design/icons';
 import { useState } from "react";
 import './detailContent.css'
 
 export default function DetailContent({item})
 {
     const desc = ['Dở tệ', 'Không hay', 'Bình thường', 'Hay', 'Tuyệt vời'];
+    console.log(item);
     const [value, setValue] = useState(1);
     return(
         <>
             <h1 style = {{marginBlock: '16px'}} className="content__title">{item.original_title}</h1>
             <div className="film-detail__buttonWrapper">
-                <Button  type="primary" shape="round" size={'large'} icon={<CaretRightOutlined />}>
+                <Button style={{marginRight: '16px'}}  type="primary" shape="round" size={'large'} icon={<CaretRightOutlined />}>
                 Xem ngay
                 </Button>
                 <Button type="primary" shape="round" size={'large'} icon={<PlusOutlined />}>
                 Danh sách
                 </Button>
             </div>
-            <div className="film-detail__statistic"  style={{marginBlock: '8px'}}>
-              
-                <div style={{color: '#FFF'}}>
-                    <span className="" style={{marginRight: '4px'}}>Đánh giá ngay:</span>
-                    <Rate tooltips={desc} onChange={setValue} value={value}  />
-                     {value ? <span style={{fontSize: '16px'}} className="ant-rate-text">{desc[value - 1]}</span> : ''}
-                </div>
-                <div style={{marginTop: '8px'}}>
-                        <span className="" style={{marginRight: '4px'}}>Điểm đánh giá:</span>
-                            <span></span>
-                    <span style={{marginRight: '8px'}}>Lượt bình chọn:</span>
-                    <span style = {{marginRight: '4px'}} className="">{item.vote_count}</span>
-                    <span style={{marginRight: '8px'}}><LikeOutlined></LikeOutlined></span>
-                </div>
+            <div className="film-detail__statistic">
+                  <span className="film-detail__statistic-point">{parseFloat(item.vote_average).toFixed(1)}</span>
+                  <div className="film-detail__statistic-rating">
+                       <div>
+                        <Rate style={{fontSize: 24}} tooltips={desc} onChange={setValue} value={value}  />
+                            {value ? <span style={{fontSize: '16px'}} className="ant-rate-text">{desc[value - 1]}</span> : ''}
+                       </div>
+                        <span style = {{marginRight: '4px'}} className="">{item.vote_count} đánh giá</span>
+                  </div>
             </div>
+            <div className="film-detail__descripton">{item.overview}</div>
+            <div></div>
          
         </>
     )
